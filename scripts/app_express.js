55c71f9aba84595bdab17946915d80c6486d298b
nipundavid <nipun.david@nagarro.com>
2019-06-11 23:15:03 +0530

    mongodb added, created db in the databas -books, rendering all data on the url

--- scripts/app_express.js
+++ scripts/app_express.js
@@ -1,17 +1,26 @@
 const express  = require("express");
-
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const bookRouter = express.Router();
 
 var port = process.env.port || 3000;
 
+const db = mongoose.connect("mongodb://localhost/books");
+const Book = require(path.resolve(__dirname, "../models/bookModel"));
+
 //router created
 bookRouter.route("/books")
     .get((req,res)=> {
-        const response = {hello : "this is my api"};
-
-        res.json(response);
+        Book.find((err,books) => {
+            if(err){
+                res.send(err)
+            }else {
+                console.log(books);
+                res.json(books);
+            }
+        });
     });
 
 //added in middleware
