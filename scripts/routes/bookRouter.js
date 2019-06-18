55d8d0cb6829c3fd0f93bb9cf1a0e1be76413f5b
nipundavid <nipun.david@nagarro.com>
2019-06-18 23:29:59 +0530

    PUT verb added in bookRouter.js

--- scripts/routes/bookRouter.js
+++ scripts/routes/bookRouter.js
@@ -24,13 +24,28 @@ function routes(Book) {
 
     bookRouter.route("/books/:bookId")
         .get((req,res)=> {
-            Book.findById(req.params.bookId, (err,books) => {
+            Book.findById(req.params.bookId, (err,book) => {
                 if(err){
-                    res.send(err)
-                }else {
-                    res.json(books);
+                    return res.send(err)
                 }
+                return res.json(book);
             });
+    })
+    .put((req,res) =>{
+        Book.findById(req.params.bookId, (err,book) => {
+            if(err){
+                return res.send(err)
+            }
+
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+
+            book.save();
+
+            return res.json(book);
+        });
     });
 
     return bookRouter;
